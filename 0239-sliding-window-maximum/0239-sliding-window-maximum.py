9ace455d1521daa9cbd7fdf6e95c0ef7c352228a
shelialia <136307144+shelialia@users.noreply.github.com>
2026-06-06 17:03:58 +0800

    Time: 211 ms (26.58%), Space: 34.7 MB (78.15%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,6 +1,23 @@
 from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+        monatonically decreasing deque
+        Why use a deque? 
+        o(1) insert/delete at front AND back of queue
+        Compared to a stack, whereby we are unable to efficiently access the "front of the stack"
+        How it works:
+        1. Deque stores indexes
+        - Front is the index with largest value in the window
+            - At each iteration, we check if front value is outdated; remove if it is.
+        - Back is for us to add "possible largests in the window"
+            - At each iteration, we compare our current value with the values at the back of the deque
+            - While value of index at back of stack < curr value, they are "outdated possible max", so we pop them
+            - Then, we add the index of our "new possible max value"
+        2. Only append result when (right + 1) >= k because it's when the window starts to move
+        3. Only if (right + 1) >= k then we move our left pointer too (window moves)
+        4. At every iteration, we move our right pointer. 
+        """
         res = []
         q = deque() # stores indexes
         left, right = 0, 0
