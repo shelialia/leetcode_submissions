99df4cea0d27ef58bc7107bad905b28b969b2658
shelialia <136307144+shelialia@users.noreply.github.com>
2026-06-06 15:59:29 +0800

    Time: 455 ms (95.9%), Space: 22.4 MB (18.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,7 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         """
-
+        1. Sort the array
+        2. Create list to track the unique triplets (cannot be repeated)
+        3. Iterate through nums:
+        - If same as previous value, skip (will get duplicate triplet)
+        - Performance enhancement: if num>0, there's no way to get sum 0
+        - 2 Pointer solution (since the array is sorted) to get the 2 indexes
+            - if target is found, we skip the duplicates on left & right
         """
         res = []
         nums.sort()
@@ -9,8 +15,8 @@ class Solution:
         for i, num in enumerate(nums):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            # if num > 0:
-            #     break
+            if num > 0: # if num>0, there's no way to get sum 0
+                break
             left, right = i + 1, len(nums) - 1
             while left < right:
                 curr_sum = num + nums[left] + nums[right]
