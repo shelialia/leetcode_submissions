8594d4483338d55b85825dd4969b7efa375101d8
shelialia <136307144+shelialia@users.noreply.github.com>
2026-06-06 13:14:40 +0800

    Time: 12 ms (54.42%), Space: 32.3 MB (27.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,6 +4,9 @@ class Solution:
         Use a set
         - insert/remove, check if in the set:
             - is O(1) on average case, but O(n) on worst case
+        Time Complexity: O(n) as we loop through once
+        - "in" check and add operation are O(1)
+        Space Complexity: O(n) in worst case, all elements are distinct
         """
         numSet = set()
         for num in nums:
