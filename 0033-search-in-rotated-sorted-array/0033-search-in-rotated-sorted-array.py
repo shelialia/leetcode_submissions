9d0fb7c9c8c65b4995aae78abb8bb40e38cfc567
shelialia <136307144+shelialia@users.noreply.github.com>
2026-06-06 22:45:20 +0800

    Time: 0 ms (100%), Space: 19.6 MB (11.19%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,23 @@
 class Solution:
     def search(self, rotatedArray: List[int], target: int) -> int:
+        """
+        Binary search spin off qn
+
+        Foundations of qn:
+        1. Condition on left <= right
+        2. if medvalue == target: return the index
+        3. Else, find out which half of the array the medpoint is in
+        - Left half of array: if medvalue > leftmost value
+            > Search leftside of med: if target between left and med
+            > Search rightside of med: if target is not
+        - Right half of array: if medvalue < rightmost value
+            > Search rightside of med: if target between med and right
+            > Search leftside of med: if target is not
+        4. Else, return -1 (value not found)
+
+        Time complexity: O(n); 2 pointer through the array
+        Space complexity: O(1); no extra space used
+        """
         left, right = 0, len(rotatedArray) - 1
 
         while left <= right:
