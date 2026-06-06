ea2fb91ddb1dac97fc6f8fab932d296a7e3715bb
shelialia <136307144+shelialia@users.noreply.github.com>
2026-06-06 13:05:39 +0800

    Time: 25 ms (86.88%), Space: 31.3 MB (94.33%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        currSum = 0
+        for num in nums:
+            if currSum < 0:
+                currSum = 0
+            currSum += num
+            maxSum = max(maxSum, currSum)
+        return maxSum
+      # "Sliding window" problem
+      # Keep track of the currSum (prefix sum before the index)
+      # If the currSum is negative, discard it (reset)
+        # because it would not help the case
+      # Otherwise, it is possibly helpful (get a bigger sum)
+        # Add it!
