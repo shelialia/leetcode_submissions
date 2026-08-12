fb838a17358ee42b04456c1c3426d9f896affc1e
shelialia <136307144+shelialia@users.noreply.github.com>
2026-08-12 17:42:34 +0800

    Time: 0 ms (100%), Space: 20.6 MB (6.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,10 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_dict = {}
 
-        # retry 4
         for i, num in enumerate(nums):
-            desired = target - num
-            if desired in num_dict:
-                return [i, num_dict[desired]]
-            else:
-                num_dict[num] = i
+            if target - num in num_dict:
+                return [num_dict[target - num], i]
+            num_dict[num] = i
