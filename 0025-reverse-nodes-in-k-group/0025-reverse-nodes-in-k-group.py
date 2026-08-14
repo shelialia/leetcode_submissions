6a58974f77373b722df0e18560a1e2df019df171
shelialia <136307144+shelialia@users.noreply.github.com>
2026-08-14 12:44:40 +0800

    Time: 0 ms (100%), Space: 20.4 MB (46.41%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -1,48 +1,44 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        to_reverse_head = head
-        to_reverse_tail = None
-        result_head = None
-        while to_reverse_head:
-            start, end = to_reverse_head, to_reverse_head
+        new_head = head
+        previous_group_tail = None
+        current_group_head = head
 
-            for i in range(k - 1):
-                end = end.next
-                if not end:
+        while current_group_head:
+            current_group_tail = current_group_head
+
+            # Find the kth node in the current group
+            for _ in range(k - 1):
+                current_group_tail = current_group_tail.next
+                if not current_group_tail:
                     break
-                    
-            if not end:
-                to_reverse_tail.next = start
+
+            # Fewer than k nodes remain
+            if not current_group_tail:
+                if previous_group_tail:
+                    previous_group_tail.next = current_group_head
                 break
-            
-            next_group = end.next
-            to_reverse_head = next_group
 
-            prev = None
-            curr = start
+            next_group_head = current_group_tail.next
 
-            if not result_head:
-                result_head = end
+            # Reverse the current group
+            previous_node = None
+            current_node = current_group_head
 
-            while curr != next_group:
-                tmp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = tmp
-            
-            if to_reverse_tail:
-                to_reverse_tail.next = end
+            while current_node != next_group_head:
+                next_node = current_node.next
+                current_node.next = previous_node
+                previous_node = current_node
+                current_node = next_node
 
-            to_reverse_tail = start
+            # current_group_tail is now the head of the reversed group
+            if current_group_head == head:
+                new_head = current_group_tail
+            else:
+                previous_group_tail.next = current_group_tail
 
+            # current_group_head is now the tail of the reversed group
+            previous_group_tail = current_group_head
+            current_group_head = next_group_head
 
-        return result_head
-
-                
-
-                
+        return new_head
