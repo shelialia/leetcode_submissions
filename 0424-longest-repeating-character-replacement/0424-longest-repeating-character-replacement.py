eed575faca74e500fcaf9864bb9640a59c88e7fe
shelialia <136307144+shelialia@users.noreply.github.com>
2026-08-19 22:40:54 +0800

    Time: 174 ms (15.43%), Space: 19.6 MB (85.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        curr_char_set = [0] * 26
-        longest_len = 0
+        # Counter array with length 26 storing the counts of each english char
+        # Sliding window problem
+        # While k < len(window) - max(highest occurrence in window)
+        # shift left pointer. otherwise, can continue expanding window to right
+        counter = [0] * 26
+        max_len = 0
         left = 0
-
         for right in range(len(s)):
-            curr_char_set[ord(s[right]) - ord("A")] += 1
-
-            while (right - left + 1) - max(curr_char_set) > k:
-                curr_char_set[ord(s[left]) - ord("A")] -= 1
+            counter[ord(s[right]) - ord("A")] += 1
+            while k < (right - left + 1) - max(counter):
+                counter[ord(s[left]) - ord("A")] -= 1
                 left += 1
-            longest_len = max(longest_len, right - left + 1)
-            
-        return longest_len
+            max_len = max(max_len, right - left + 1)
+        return max_len
