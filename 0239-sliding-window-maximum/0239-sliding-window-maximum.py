40e2667f595075f9903f84dad46b704674f09979
shelialia <136307144+shelialia@users.noreply.github.com>
2026-08-20 13:21:55 +0800

    Time: 171 ms (87.34%), Space: 35.5 MB (24.99%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,10 +3,15 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         """
         monatonically decreasing deque
-        Why use a deque? 
-        o(1) insert/delete at front AND back of queue
-        Compared to a stack, whereby we are unable to efficiently access the "front of the stack"
-        How it works:
+        Why use a deque? We want to have these features
+        1. Insert at back of queue
+        2. Pop from front of queue
+        => Queue features so far
+        3. Pop at the back of the queue => not allowed in queue structure
+        - queues only allow to pop from the front
+        => use a deque which allow us to pop/append from both front and back of queue
+        
+        How it works: Maintain a monatonically decreasing queue
         1. Deque stores indexes
         - Front is the index with largest value in the window
             - At each iteration, we check if front value is outdated; remove if it is.
@@ -14,29 +19,27 @@ class Solution:
             - At each iteration, we compare our current value with the values at the back of the deque
             - While value of index at back of stack < curr value, they are "outdated possible max", so we pop them
             - Then, we add the index of our "new possible max value"
-        2. Only append result when (right + 1) >= k because it's when the window starts to move
-        3. Only if (right + 1) >= k then we move our left pointer too (window moves)
-        4. At every iteration, we move our right pointer. 
+        2. Only append result when (right + 1) >= k because it's when window size hits k
+        3. Only if (right + 1) >= k then we increment our left pointer too (window moves)
+        4. At every iteration, we increment our right pointer. 
 
         Time Complexity: O(n); The pop and append operations are amortized O(1) on average
         Space Complexity: O(n); Worst case where nums is strictly decreasing then every element is added to the queue AND the window size k == size of array nums
         """
+        queue = deque() # Monatonically decreasing queue storing indexes corresponding to the value
         res = []
-        q = deque() # stores indexes
-        left, right = 0, 0
 
-        while right < len(nums):
-            if q and left > q[0]:
-                q.popleft()
-            while q and nums[q[-1]] < nums[right]:
-                q.pop()
-            q.append(right)
-
-            if (right + 1) >= k:
-                left += 1
-                res.append(nums[q[0]])
+        left = 0
+        for right in range(len(nums)):
+            while queue and nums[queue[-1]] < nums[right]:
+                queue.pop()
+            queue.append(right)
             
-            right += 1
+            if (right + 1) >= k:
+                res.append(nums[queue[0]])
+                if queue[0] == left:
+                    queue.popleft()
+                left += 1
         return res
-
-
+            
+        
