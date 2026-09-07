41ef10ea238d640aca9844a55e1623cf8af8ab44
shelialia <136307144+shelialia@users.noreply.github.com>
2026-09-07 17:06:20 +0800

    Time: 7 ms (45.18%), Space: 19.3 MB (77.8%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -11,19 +11,13 @@ class Solution:
         domain_to_count = defaultdict(int)
         for cpdomain in cpdomains:
             count, domain = cpdomain.split()
+            parts = domain.split(".")
 
-            domains = []
-            domains.append(domain)
+            for i in range(len(parts)):
+                subdomain = ".".join(parts[i:])
+                domain_to_count[subdomain] += int(count)
 
-            # If in format rep d1.d2.d3, extract d2.d3
-            split_domains = domain.split(".")
-            if len(split_domains) > 2:
-                second_third_domain = split_domains[1] + "." + split_domains[2]
-                domains.append(second_third_domain)
-            
-            # For both formats rep d1.d2 and rep d1.d2.d3, extract d2 and d3 respectively
-            domains.append(split_domains[-1])
-            
-            for d in domains:
-                domain_to_count[d] += int(count)
-        return [str(count) + " " + domain for domain, count in domain_to_count.items()]
+        return [
+            f"{count} {domain}"
+            for domain, count in domain_to_count.items()
+        ]
