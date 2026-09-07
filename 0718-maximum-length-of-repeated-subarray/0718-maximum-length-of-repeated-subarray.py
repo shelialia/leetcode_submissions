68cbcaa9ed61edbb156b38982f0ea90a01f93779
shelialia <136307144+shelialia@users.noreply.github.com>
2026-09-07 17:39:35 +0800

    Time: 1440 ms (20.43%), Space: 44.4 MB (28.33%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -1,6 +1,7 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        # Let dp[i][j] = Longest subsuequence
+        # dp[i][j] = length of the longest common subarray
+        # ending at nums1[i - 1] and nums2[j - 1]
         n = len(nums1)
         m = len(nums2)
         dp = [[0] * (m + 1) for _ in range(n + 1)]
@@ -12,5 +13,37 @@ class Solution:
                     dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i][j])
                     max_len = max(max_len, dp[i][j])
         return max_len
-        
+
+# nums1 = [1, 2]
+# nums2 = [3, 1]
+# dp = [[0, 0, 0]
+#       [0, 0, 0]
+#       [0, 0, 0]]
+
+# i = 1, j = 1: 
+# dp = [[0, 0, 0]
+#       [0, 0, 0]
+#       [0, 0, 0]]
+# max_len = 0
+
+# i = 1, j = 2: 
+# dp = [[0, 0, 0]
+#       [0, 0, 1]
+#       [0, 0, 0]]
+# max_len = 1
+
+# i = 2, j = 1: 
+# dp = [[0, 0, 0]
+#       [0, 0, 1]
+#       [0, 0, 0]]
+# max_len = 1
+
+# i = 2, j = 2: 
+# dp = [[0, 0, 0]
+#       [0, 0, 1]
+#       [0, 0, 0]]
+# max_len = 1
+
+# => return max_len = 1
+
         
