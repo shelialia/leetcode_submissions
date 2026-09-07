727278c1f1206ad5278ca1ab124013f2ff0667c5
shelialia <136307144+shelialia@users.noreply.github.com>
2026-09-07 16:52:31 +0800

    Time: 4 ms (68.71%), Space: 19.3 MB (41.25%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        """
+        Slice the domain into subdomains
+        1. Whole domain
+        2. Second domain + third domain
+        3. Third domain
+        Store a hashmap of domain -> count
+        """
+        domain_to_count = defaultdict(int)
+        for cpdomain in cpdomains:
+            count, domain = cpdomain.split()
+
+            domains = []
+            domains.append(domain)
+            split_domains = cpdomain.split(".")
+            if len(split_domains) > 2:
+                second_third_domain = split_domains[1] + "." + split_domains[2]
+                domains.append(second_third_domain)
+            
+            domains.append(split_domains[-1])
+            
+            for d in domains:
+                domain_to_count[d] += int(count)
+        return [str(count) + " " + domain for domain, count in domain_to_count.items()]
