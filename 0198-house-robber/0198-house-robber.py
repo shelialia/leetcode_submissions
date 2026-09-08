6bb29f260bdc0d3dc6f4939486453284c06a1889
shelialia <136307144+shelialia@users.noreply.github.com>
2026-09-08 13:39:49 +0800

    Time: 0 ms (100%), Space: 19.1 MB (90.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,12 +5,12 @@ class Solution:
         elif len(nums) == 2:
             return max(nums[0], nums[1])
         
-        dp = [0] * (len(nums) + 1)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        first, second = nums[0], max(nums[0], nums[1])
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
-        return max(dp[-1], dp[-2])
+            temp = max(first + nums[i], second)
+            first = second
+            second = temp
+        return second
 
 
 """
@@ -22,7 +22,16 @@ nums = [2,7,9,3,1]
 Rob 2 + 9 + 1 = 12 (Output = 12)
 Rob 7 + 3 = 10
 
-dp[i] = max amount you can rob
-dp[i] = max(rob i - 2 house + i house, rob i - 1 house + skip i house)
-dp[i] = max(dp[i - 2] + num[i], dp[i - 1])
+dp[i] = max amount you can rob from house 0..i
+
+1. Rob house i:
+   We cannot rob i-1, so:
+   dp[i-2] + nums[i]
+
+2. Skip house i:
+   Best remains:
+   dp[i-1]
+
+Therefore:
+dp[i] = max(dp[i-2] + nums[i], dp[i-1])
 """
