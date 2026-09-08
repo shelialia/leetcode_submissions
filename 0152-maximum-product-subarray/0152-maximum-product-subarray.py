d976c85e0bf5db23961e5f02f08c88ac36d848a6
shelialia <136307144+shelialia@users.noreply.github.com>
2026-09-08 23:26:16 +0800

    Time: 4 ms (58.86%), Space: 19.9 MB (45.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,15 +1,62 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        curr_min = nums[0]
+        curr_max = nums[0]
         res = nums[0]
-        curMin, curMax = 1, 1
 
-        for n in nums:
-            if n == 0:
-                res = max(res, 0)
-                curMin, curMax = 1, 1
-                continue
-            tmp = curMin * n
-            curMin = min(curMin * n, curMax * n, n)
-            curMax = max(tmp, curMax * n, n)
-            res = max(curMax, res)
+        # Include nums[i] itself because we may want to start a new subarray at i instead of extending the previous one.
+        for i in range(1, len(nums)):
+            prev_min = curr_min
+            prev_max = curr_max
+
+            curr_min = min(
+                nums[i],
+                prev_min * nums[i],
+                prev_max * nums[i]
+            )
+            curr_max = max(
+                nums[i],
+                prev_min * nums[i],
+                prev_max * nums[i]
+            )
+            res = max(res, curr_max)
         return res
+
+"""
+nums = [2,3,-2,4]
+            ^
+min_pdt = -48
+max_pdt = 6
+
+Naive:
+Generate all O(n²) subarrays.
+If recalculating each product from scratch:
+Time: O(n³)
+
+DP idea:
+
+Because multiplying by a negative number can swap the maximum and minimum,
+we must track both.
+
+max_dp[i] = maximum product of a subarray ending at i
+min_dp[i] = minimum product of a subarray ending at i
+
+For nums[i]:
+
+max_dp[i] = max(
+    nums[i],
+    max_dp[i-1] * nums[i],
+    min_dp[i-1] * nums[i]
+)
+
+min_dp[i] = min(
+    nums[i],
+    max_dp[i-1] * nums[i],
+    min_dp[i-1] * nums[i]
+)
+
+Take the maximum max_dp[i] over all i.
+
+Time: O(n)
+Space: O(1) if only previous max/min are stored
+"""
